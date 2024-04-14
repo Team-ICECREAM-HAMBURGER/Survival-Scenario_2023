9acdbce86d718daa93f03f84296c523a54151649
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2024-04-15 00:43:22 +0900

    부상을 입음 - 1
    부상을 입을 경우, PlayerStatusEffectInjured의 OnInjuredEffectEvent() 메서드를 호출한다.
    
    이벤트 결과를 EventResult()로 출력한다.
    
    부상을 입었을 때 일어나는 이벤트는 PlayerStatusEffectInjured에서 구현.
    -> 일정 시간동안 이벤트가 유지되어야 한다.
    -> 상태 수치 감소량을 증가시켜야 한다.
    -> 이동 이벤트는 잠긴다.
    -> 약초로 만든 의약품을 사용하면 일정 시간이 줄어든다.

--- Assets/Script/GameRandomEvent/GameRandomEventSearchInjured.cs
+++ Assets/Script/GameRandomEvent/GameRandomEventSearchInjured.cs
@@ -1,37 +1,55 @@
+using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
-public class GameRandomEventSearchInjured : MonoBehaviour {
-    // private void InjuredEvent() {
-    //     // Debug
-    //     Debug.Log("Injured");
-    // }
-    //
-    // private (string title, StringBuilder content) InjuredResult() {
-    //     this.content.Clear();
-    //     
-    //     this.content.Append("- 결과\n");
-    //     this.content.Append("탐색 도중 위험에 빠졌다.\n");
-    //     this.content.Append("가까스로 돌아오기는 했지만 부상을 입고 말았다.\n");
-    //     this.content.Append("부상 회복까지 일(텀)이 걸린다.\n");
-    //     this.content.Append("부상 회복이 먼저다. 의약품을 만들고 휴식을 취하자.\n");
-    //     
-    //     this.content.Append("\n");
-    //     
-    //     this.content.Append("- 스테이터스 잔여량\n");
-    //     this.content.Append($"체력: %\n");
-    //     this.content.Append($"체온: %\n");
-    //     this.content.Append($"수분: %\n");
-    //     this.content.Append($"열량: %\n");
-    //     
-    //     this.content.Append("\n");
-    //     
-    //     this.content.Append("- 획득한 아이템\n");
-    //     
-    //     this.content.Append("\n");
-    //     
-    //     this.content.Append("- 소모한 아이템\n");
-    //     this.content.Append("없음\n");
-    //     
-    //     return (this.title, this.content);
-    // }
+public class GameRandomEventSearchInjured : MonoBehaviour, IGameRandomEvent {
+    public float Weight { get; private set; }
+
+    private string title;
+    private StringBuilder content;
+
+
+    private void Init() {
+        this.Weight = 0f;
+        this.content = new();
+    }
+
+    private void Awake() {
+        Init();
+    }
+    
+    public void Event() {
+        Debug.Log("InjuredEvent");
+        
+        PlayerStatusEffectInjured.OnInjuredEffectEvent();   // 스테이터스 상태 업데이트; 상태 이상 적용
+        
+        EventResult();
+    }
+
+    public void EventResult() {
+        this.title = "탐색 결과";
+        this.content.Clear();
+        
+        this.content.Append("- 결과\n");
+        this.content.Append("탐색 도중 부주의로 인해 부상을 입고 말았다.\n");
+        this.content.Append("이동보다는 부상 회복이 우선이다. 휴식을 취하며 의약품을 구해보자.\n");
+
+        this.content.Append("\n");
+        
+        this.content.Append("부상 회복까지 일(텀) 남음.\n");
+
+        this.content.Append("\n");
+        
+        this.content.Append("- 스테이터스 잔여량\n");
+        this.content.Append($"체력: %\n");
+        this.content.Append($"체온: %\n");
+        this.content.Append($"수분: %\n");
+        this.content.Append($"열량: %\n");
+        
+        this.content.Append("\n");
+
+        this.content.Append("부상 상태 이상 효과가 적용됨.\n");
+        
+        PlayerBehaviourSearch.OnSearchEventUpdateView(this.title, this.content.ToString());
+    }
 }
--- Assets/Script/Player/PlayerStatusEffect/PlayerStatusEffectInjured.cs
+++ Assets/Script/Player/PlayerStatusEffect/PlayerStatusEffectInjured.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
 
-public class PlayerStatusEffectInjured : IPlayerStatusEffect {
+public class PlayerStatusEffectInjured : MonoBehaviour, IPlayerStatusEffect {
     public string StatusEffectName { get; } = "부상";
     public GameTypeStatusEffect GameTypeStatusEffect { get; } = GameTypeStatusEffect.INJURED;
     public int DurationTerm { get; private set; }
+
+    public delegate void InjuredEffectEventHandler();
+    public static InjuredEffectEventHandler OnInjuredEffectEvent;
+
     
+    private void Init() {
+        OnInjuredEffectEvent += Event;
+    }
+
+    private void Awake() {
+        Init();
+    }
     
     public void Event() {
+        // TODO: 일정 시간동안 상태가 유지되어야 함. (일정 시간은 무작위 범위) -> 상태 수치 감소량이 증가함. -> 이동 불가
+        // TODO: 약초로 만든 의약품을 사용하면 일정 시간이 줄어들어야 함. (줄어드는 양은 고정적)
+        
         // GameInfoControl.OnTimeUpdateEvent += DurationTermUpdate;
         //
         // this.DurationTerm = Random.Range(3, 8) * 500;
