bab48a33a40568b8df1e95817428ac7075466a99
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2024-06-16 11:21:12 +0900

    DB & Web API - 1
    라즈베리 파이에 구축한 DB 서버와 연동하여 게임 세이브 데이터를 저장/불러오는 기능 구현 중.
    
    Web API에는 Python을 사용하는 Flask 프레임워크 적용.

---
+++ Server/config.py
@@ -0,0 +1,6 @@
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'your_mysql_user'
+    MYSQL_PASSWORD = 'your_mysql_password'
+    MYSQL_DB = 'survival_scenario'
+    MYSQL_CURSORCLASS = 'DictCursor'
