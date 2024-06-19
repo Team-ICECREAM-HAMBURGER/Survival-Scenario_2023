4bc176cfa6363af59bb2fc1c1b2458f9452f5023
Seung-Ki Kim <kimsk200012@gmail.com>
2024-06-19 20:01:23 +0900

    DB & Web API - 2
    불필요한 파일 제거.
    
    라즈베리 파이에 넣어야 하는 걸 왜 Unity 프로젝트 폴더에 넣었을까.......

--- Server/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'your_mysql_user'
-    MYSQL_PASSWORD = 'your_mysql_password'
-    MYSQL_DB = 'survival_scenario'
-    MYSQL_CURSORCLASS = 'DictCursor'
